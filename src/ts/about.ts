4809136264fe1a5f3e8647b81229ff6ee76d5bf1
Shogo-Sekine <gan.gun.kyosho@gmail.com>
2024-01-22 15:22:35 +0900

    add about page

---
+++ src/ts/about.ts
@@ -0,0 +1,7 @@
+// about.html内で読み込むcssをimport
+import "../styles/about.scss";
+import "../styles/reset.scss";
+
+// about.html内で実行するJavaScript(TypeScript)
+const about: string = "about world";
+console.log(about);
