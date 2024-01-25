be933be468f79b169c087df59eae0bedf7470334
Shogo-Sekine <gan.gun.kyosho@gmail.com>
2024-01-25 11:14:55 +0900

    fix

---
+++ src/ts/app-ads.ts
@@ -0,0 +1,7 @@
+// about.html内で読み込むcssをimport
+// import "../styles/about.scss";
+// import "../styles/reset.scss";
+
+// about.html内で実行するJavaScript(TypeScript)
+const appads: string = "app-ads world";
+console.log(appads);
